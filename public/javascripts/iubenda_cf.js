ad42f2b0ee2840ebb1fda9918a5dbc2b432bf590
Mimmo Vele <mimmo@iubenda.com>
2012-09-28 20:37:00 +0200

    First App Code targeting staging enviroment

--- public/javascripts/iubenda_cf.js
+++ public/javascripts/iubenda_cf.js
@@ -2,25 +2,10 @@ CloudFlare.define("iubenda",
     [       "iubenda/config", "cloudflare/jquery1.7"],
     function(config,                $)
     {
-		/*function addBadge(count){
-            var cdnPath = "//ajax.cloudflare.com/cdn-cgi/nexp/apps/countersback/";
-            var div = $("<div>")
-            $("<img>",{
-                src: cdnPath + "images/counter.png",
-                "class": "countersback"
-            }).appendTo(div);
-            $("<span>", {"style": "position:relative; top:-20px; left:-50px;")
-                .text(count).appendTo(div)
-            div.appendTo("body")
-        }
-        $.getJSON(
-            "http://countersback.herokuapp.com/hit?callback=?",
-            {domain_id: config && config.domain_id},
-            function(data){
-                if (this.data.count){
-                    addBadge(this.data.count)
-                }
-            }
-        )*/
+		function addBadge(){
+			var embeddedCode = "<a href=\"http://staging.mater.iubenda.com/privacy-policy/1/"+config.domain_id+"\" class=\"iubenda-white iubenda-embed iub-anchor\" title=\"Privacy Policy\">Privacy Policy</a><script type=\"text/javascript\">(function (w,d) {var loader = function () {var s = d.createElement(\"script\"), tag = d.getElementsByTagName(\"script\")[0]; s.src = \"http://staging.mater.iubenda.com/assets/cdn/iubenda.js\"; tag.parentNode.insertBefore(s,tag);}; loader();})(window, document);</script>";
+			$("body").append(embeddedCode);
+		}
+		addBadge();
     }
 )
