f11304199bd55fadeb0ca7e557506d454eec046a
Mimmo Vele <mimmo@iubenda.com>
2012-10-06 16:47:51 +0200

    iubenda_cf.js does not add badge if other badge or embedding code is found on page

--- public/javascripts/iubenda_cf.js
+++ public/javascripts/iubenda_cf.js
@@ -3,9 +3,14 @@ CloudFlare.define("iubenda",
     function(config,                $)
     {
 		function addBadge(){
+			console.log("adding...")
 			var embeddedCode = "<a href=\"http://staging.mater.iubenda.com/privacy-policy/1/"+config.domain_id+"\" class=\"iubenda-white iubenda-embed iub-anchor\" title=\"Privacy Policy\">Privacy Policy</a><script type=\"text/javascript\">(function (w,d) {var loader = function () {var s = d.createElement(\"script\"), tag = d.getElementsByTagName(\"script\")[0]; s.src = \"http://staging.mater.iubenda.com/assets/cdn/iubenda.js\"; tag.parentNode.insertBefore(s,tag);}; loader();})(window, document);</script>";
 			$("body").append(embeddedCode);
 		}
-		addBadge();
+		function lookForBadge(){
+			console.log("looking for...")
+			if(!$(".iubenda-ibadge")[0] && !$(".iubenda-embed")[0] && !$("#iubenda-embed")[0]){addBadge();}
+		}
+		lookForBadge();
     }
 )
