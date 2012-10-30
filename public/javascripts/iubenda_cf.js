b91ceeac25bf3dc49131208617962426118c9417
mimmovele <mimmo@iubenda.com>
2012-10-31 00:39:53 +0100

    Fix on iubenda_cf.js for console.

--- public/javascripts/iubenda_cf.js
+++ public/javascripts/iubenda_cf.js
@@ -1,30 +1,30 @@
 CloudFlare.define("iubenda",
-    [       "iubenda/config", "cloudflare/jquery1.7"],
-    function(config,                $)
+    [       "iubenda/config", "cloudflare/jquery1.7","cloudflare/console"],
+    function(config,                $,console)
     {
 		function addBadge(){
-			console.log("adding...")
+			console.log("adding...");
 			var embeddedCode = "<a href=\"http://staging.mater.iubenda.com/privacy-policy/1/"+config.domain_id+"\" class=\"iubenda-white iubenda-embed iub-anchor\" title=\"Privacy Policy\">Privacy Policy</a><script type=\"text/javascript\">(function (w,d) {var loader = function () {var s = d.createElement(\"script\"), tag = d.getElementsByTagName(\"script\")[0]; s.src = \"http://staging.mater.iubenda.com/assets/cdn/iubenda.js\"; tag.parentNode.insertBefore(s,tag);}; loader();})(window, document);</script>";
 			$("body").append(embeddedCode);
-			console.log("added!")
+			console.log("added!");
 		}
 		function lookForBadge(){
-			console.log("looking for...")
+			console.log("looking for adding badge...");
 			if(!config.skip_badge){
 				if(!$(".iubenda-ibadge")[0] && !$(".iubenda-embed")[0] && !$("#iubenda-embed")[0]){
-					console.log("other badge or pp embedded not found. Calling addBadge()...")
+					console.log("other badge or pp embedded not found. Calling addBadge()...");
 					addBadge();
 				}else{
-					console.log("other iubenda components found:")
-					console.log("!$('.iubenda-ibadge')[0]")
-					console.log(!$(".iubenda-ibadge")[0])
-					console.log("!$('.iubenda-embed')[0]")
-					console.log(!$(".iubenda-embed")[0])
-					console.log("!$('#iubenda-embed')[0]")
-					console.log(!$("#iubenda-embed")[0])
+					console.log("other iubenda components found:");
+					console.log("!$('.iubenda-ibadge')[0]");
+					console.log(!$(".iubenda-ibadge")[0]);
+					console.log("!$('.iubenda-embed')[0]");
+					console.log(!$(".iubenda-embed")[0]);
+					console.log("!$('#iubenda-embed')[0]");
+					console.log(!$("#iubenda-embed")[0]);
 				}
 			}else{
-				console.log("badge skipped by configuration!")
+				console.log("badge skipped by configuration!");
 			}
 		}
 		lookForBadge();
