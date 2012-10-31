e62bd638733348e461817cb7de60c6da735953f2
mimmovele <mimmo@iubenda.com>
2012-10-31 19:29:06 +0100

    Minor changes on iubenda_cf.js and console logging.

--- public/javascripts/iubenda_cf.js
+++ public/javascripts/iubenda_cf.js
@@ -1,16 +1,17 @@
 CloudFlare.define("iubenda",
-    [       "iubenda/config", "cloudflare/jquery1.7","cloudflare/console"],
-    function(config,                $,console)
+    [       "iubenda/config", "cloudflare/jquery1.7"],
+    function(_config,$)
     {
 		function addBadge(){
 			console.log("adding...");
-			var embeddedCode = "<a href=\"http://staging.mater.iubenda.com/privacy-policy/1/"+config.domain_id+"\" class=\"iubenda-white iubenda-embed iub-anchor\" title=\"Privacy Policy\">Privacy Policy</a><script type=\"text/javascript\">(function (w,d) {var loader = function () {var s = d.createElement(\"script\"), tag = d.getElementsByTagName(\"script\")[0]; s.src = \"http://staging.mater.iubenda.com/assets/cdn/iubenda.js\"; tag.parentNode.insertBefore(s,tag);}; loader();})(window, document);</script>";
+			var embeddedCode = "<a href=\"http://staging.mater.iubenda.com/privacy-policy/1/"+_config.domain_id+"\" class=\"iubenda-white iubenda-embed iub-anchor\" title=\"Privacy Policy\">Privacy Policy</a><script type=\"text/javascript\">(function (w,d) {var loader = function () {var s = d.createElement(\"script\"), tag = d.getElementsByTagName(\"script\")[0]; s.src = \"http://staging.mater.iubenda.com/assets/cdn/iubenda.js\"; tag.parentNode.insertBefore(s,tag);}; loader();})(window, document);</script>";
+			console.log("embedding code: "+embeddedCode);
 			$("body").append(embeddedCode);
 			console.log("added!");
 		}
 		function lookForBadge(){
 			console.log("looking for adding badge...");
-			if(!config.skip_badge){
+			if(!_config.skip_badge){
 				if(!$(".iubenda-ibadge")[0] && !$(".iubenda-embed")[0] && !$("#iubenda-embed")[0]){
 					console.log("other badge or pp embedded not found. Calling addBadge()...");
 					addBadge();
