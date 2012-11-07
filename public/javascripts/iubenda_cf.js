b3c0381fac118d09a0bff814a1c6aaf0befd509d
mimmovele <mimmo@iubenda.com>
2012-11-07 17:43:44 +0100

    Console logging removed. Switched to production env.

--- public/javascripts/iubenda_cf.js
+++ public/javascripts/iubenda_cf.js
@@ -3,33 +3,18 @@ CloudFlare.define("iubenda",
     function(_config,$)
     {		
 		function addBadge(){
-			console.log("adding...");
 			var protocol = window.location.protocol != "https:" ? "http:" : "https:";
-			console.log("using protocol: "+protocol);
-			var embeddedCode = "<a href=\""+protocol+"//staging.mater.iubenda.com/privacy-policy/1/"+_config.domain_id+"\" class=\"iubenda-white iubenda-embed no-brand iub-anchor\" title=\"Privacy Policy\">Privacy Policy</a><script type=\"text/javascript\">(function (w,d) {var loader = function () {var s = d.createElement(\"script\"), tag = d.getElementsByTagName(\"script\")[0]; s.src = \""+protocol+"//staging.mater.iubenda.com/assets/cdn/iubenda.js\"; tag.parentNode.insertBefore(s,tag);}; loader();})(window, document);</script>";
-			console.log("embedding code: "+embeddedCode);
+			var embeddedCode = "<a href=\""+protocol+"//www.iubenda.com/privacy-policy/1/"+_config.domain_id+"\" class=\"iubenda-white iubenda-embed no-brand iub-anchor\" title=\"Privacy Policy\">Privacy Policy</a><script type=\"text/javascript\">(function (w,d) {var loader = function () {var s = d.createElement(\"script\"), tag = d.getElementsByTagName(\"script\")[0]; s.src = \""+protocol+"//cdn.iubenda.com/iubenda.js\"; tag.parentNode.insertBefore(s,tag);}; loader();})(window, document);</script>";
 			$("body").append(embeddedCode);
-			console.log("added!");
 		}
 		function lookForBadge(){
-			console.log("loaded version 0.0.13.")
-			console.log("looking for adding badge...");
-			console.log("_config.skip_badge: "+_config.skip_badge);
-			console.log("_config.skip_badge type:"+typeof(_config.skip_badge));
 			if(_config.skip_badge=="true"){
-				console.log("badge skipped by configuration!");
+				//console.log("badge skipped by configuration!");
 			}else{
 				if(!$(".iubenda-ibadge")[0] && !$(".iubenda-embed")[0] && !$("#iubenda-embed")[0]){
-					console.log("other badge or pp embedded not found. Calling addBadge()...");
 					addBadge();
 				}else{
-					console.log("other iubenda components found:");
-					console.log("!$('.iubenda-ibadge')[0]");
-					console.log(!$(".iubenda-ibadge")[0]);
-					console.log("!$('.iubenda-embed')[0]");
-					console.log(!$(".iubenda-embed")[0]);
-					console.log("!$('#iubenda-embed')[0]");
-					console.log(!$("#iubenda-embed")[0]);
+					//console.log("other iubenda components found:");
 				}
 			}
 		}
